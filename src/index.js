1075bf2901529dbe6d17e20bd13a492ce9ead7b6
Jan Tuđan <jantudjan99@gmail.com>
2021-06-19 20:47:23 +0200

    dodane rute za sve dogadaje

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ app.post ('/galerija', async (req , res) => {
     let  galerija = req.body;
 
     console.log(galerija)
-    let result = await db.collection('7wonders').insertOne(galerija);
+    let result = await db.collection('galerija').insertOne(galerija);
     if (result.insertedCount == 1) {
         res.send({
             status: 'success',
@@ -80,12 +80,12 @@ app.post ('/galerija', async (req , res) => {
     console.log(result);
 });
 
-app.post ('/7wonders', async (req , res) => {
+app.post ('/arkhamhorror', async (req , res) => {
     let db = await connect();
     let  dogadaji = req.body;
 
     console.log(dogadaji)
-    let result = await db.collection('7wonders').insertOne(dogadaji);
+    let result = await db.collection('arkhamhorror').insertOne(dogadaji);
     if (result.insertedCount == 1) {
         res.send({
             status: 'success',
@@ -101,12 +101,313 @@ app.post ('/7wonders', async (req , res) => {
     console.log(result);
 });
 
-/*app.get('/registracija',  (req, res) => {
-    let podaci = req.body; 
+app.get ('/arkhamhorror', async (req , res) => {
+    let db = await connect();
+  
+    let cursor = await db.collection('arkhamhorror').find();
+    let results = await cursor.toArray();
 
-    res.send(podaci);
-})*/
+    res.json(results);
+});
 
+app.post ('/bloodrage', async (req , res) => {
+    let db = await connect();
+    let  dogadaji = req.body;
+
+    console.log(dogadaji)
+    let result = await db.collection('bloodrage').insertOne(dogadaji);
+    if (result.insertedCount == 1) {
+        res.send({
+            status: 'success',
+            id: result.insertedId,
+        });
+    } 
+    else {
+        res.send({
+            status: 'fail',
+        });
+    }
+    
+    console.log(result);
+});
+
+app.get ('/bloodrage', async (req , res) => {
+    let db = await connect();
+  
+    let cursor = await db.collection('bloodrage').find();
+    let results = await cursor.toArray();
+
+    res.json(results);
+});
+app.post ('/catan', async (req , res) => {
+    let db = await connect();
+    let  dogadaji = req.body;
+
+    console.log(dogadaji)
+    let result = await db.collection('catan').insertOne(dogadaji);
+    if (result.insertedCount == 1) {
+        res.send({
+            status: 'success',
+            id: result.insertedId,
+        });
+    } 
+    else {
+        res.send({
+            status: 'fail',
+        });
+    }
+    
+    console.log(result);
+});
+
+app.get ('/catan', async (req , res) => {
+    let db = await connect();
+  
+    let cursor = await db.collection('catan').find();
+    let results = await cursor.toArray();
+
+    res.json(results);
+});
+
+app.post ('/dnd', async (req , res) => {
+    let db = await connect();
+    let  dogadaji = req.body;
+
+    console.log(dogadaji)
+    let result = await db.collection('dnd').insertOne(dogadaji);
+    if (result.insertedCount == 1) {
+        res.send({
+            status: 'success',
+            id: result.insertedId,
+        });
+    } 
+    else {
+        res.send({
+            status: 'fail',
+        });
+    }
+    
+    console.log(result);
+});
+
+app.get ('/dnd', async (req , res) => {
+    let db = await connect();
+  
+    let cursor = await db.collection('dnd').find();
+    let results = await cursor.toArray();
+
+    res.json(results);
+});
+
+app.post ('/lostcities', async (req , res) => {
+    let db = await connect();
+    let  dogadaji = req.body;
+
+    console.log(dogadaji)
+    let result = await db.collection('lostcities').insertOne(dogadaji);
+    if (result.insertedCount == 1) {
+        res.send({
+            status: 'success',
+            id: result.insertedId,
+        });
+    } 
+    else {
+        res.send({
+            status: 'fail',
+        });
+    }
+    
+    console.log(result);
+});
+
+app.get ('/lostcities', async (req , res) => {
+    let db = await connect();
+  
+    let cursor = await db.collection('lostcities').find();
+    let results = await cursor.toArray();
+
+    res.json(results);
+});
+
+app.post ('/mtg', async (req , res) => {
+    let db = await connect();
+    let  dogadaji = req.body;
+
+    console.log(dogadaji)
+    let result = await db.collection('mtg').insertOne(dogadaji);
+    if (result.insertedCount == 1) {
+        res.send({
+            status: 'success',
+            id: result.insertedId,
+        });
+    } 
+    else {
+        res.send({
+            status: 'fail',
+        });
+    }
+    
+    console.log(result);
+});
+
+app.get ('/mtg', async (req , res) => {
+    let db = await connect();
+  
+    let cursor = await db.collection('mtg').find();
+    let results = await cursor.toArray();
+
+    res.json(results);
+});
+
+app.post ('/risk', async (req , res) => {
+    let db = await connect();
+    let  dogadaji = req.body;
+
+    console.log(dogadaji)
+    let result = await db.collection('risk').insertOne(dogadaji);
+    if (result.insertedCount == 1) {
+        res.send({
+            status: 'success',
+            id: result.insertedId,
+        });
+    } 
+    else {
+        res.send({
+            status: 'fail',
+        });
+    }
+    
+    console.log(result);
+});
+
+app.get ('/risk', async (req , res) => {
+    let db = await connect();
+  
+    let cursor = await db.collection('risk').find();
+    let results = await cursor.toArray();
+
+    res.json(results);
+});
+
+app.post ('/pokemon', async (req , res) => {
+    let db = await connect();
+    let  dogadaji = req.body;
+
+    console.log(dogadaji)
+    let result = await db.collection('pokemon').insertOne(dogadaji);
+    if (result.insertedCount == 1) {
+        res.send({
+            status: 'success',
+            id: result.insertedId,
+        });
+    } 
+    else {
+        res.send({
+            status: 'fail',
+        });
+    }
+    
+    console.log(result);
+});
+
+app.get ('/pokemon', async (req , res) => {
+    let db = await connect();
+  
+    let cursor = await db.collection('pokemon').find();
+    let results = await cursor.toArray();
+
+    res.json(results);
+});
+
+app.post ('/splendor', async (req , res) => {
+    let db = await connect();
+    let  dogadaji = req.body;
+
+    console.log(dogadaji)
+    let result = await db.collection('splendor').insertOne(dogadaji);
+    if (result.insertedCount == 1) {
+        res.send({
+            status: 'success',
+            id: result.insertedId,
+        });
+    } 
+    else {
+        res.send({
+            status: 'fail',
+        });
+    }
+    
+    console.log(result);
+});
+
+app.get ('/splendor', async (req , res) => {
+    let db = await connect();
+  
+    let cursor = await db.collection('splendor').find();
+    let results = await cursor.toArray();
+
+    res.json(results);
+});
+
+app.post ('/uno', async (req , res) => {
+    let db = await connect();
+    let  dogadaji = req.body;
+
+    console.log(dogadaji)
+    let result = await db.collection('uno').insertOne(dogadaji);
+    if (result.insertedCount == 1) {
+        res.send({
+            status: 'success',
+            id: result.insertedId,
+        });
+    } 
+    else {
+        res.send({
+            status: 'fail',
+        });
+    }
+    
+    console.log(result);
+});
+
+app.get ('/uno', async (req , res) => {
+    let db = await connect();
+  
+    let cursor = await db.collection('uno').find();
+    let results = await cursor.toArray();
+
+    res.json(results);
+});
+
+app.post ('/yugioh', async (req , res) => {
+    let db = await connect();
+    let  dogadaji = req.body;
+
+    console.log(dogadaji)
+    let result = await db.collection('yugioh').insertOne(dogadaji);
+    if (result.insertedCount == 1) {
+        res.send({
+            status: 'success',
+            id: result.insertedId,
+        });
+    } 
+    else {
+        res.send({
+            status: 'fail',
+        });
+    }
+    
+    console.log(result);
+});
+
+app.get ('/yugioh', async (req , res) => {
+    let db = await connect();
+  
+    let cursor = await db.collection('yugioh').find();
+    let results = await cursor.toArray();
+
+    res.json(results);
+});
 
 app.listen(port, () => console.log(`\n\nhttp://localhost:${port}/\n\n`));
 
